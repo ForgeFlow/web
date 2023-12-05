8e2d3d9f3c0f63e7d12e9c65358790b90d94bf5e
BernatPForgeFlow <bernat.puig@forgeflow.com>
2023-12-05 09:45:59 +0100

    [IMP] web_widget_bokeh_chart: Do not refresh two times

--- web_widget_bokeh_chart/static/src/js/web_widget_bokeh_json_chart.esm.js
+++ web_widget_bokeh_chart/static/src/js/web_widget_bokeh_json_chart.esm.js
@@ -2,23 +2,24 @@
 
 import {loadBundle} from "@web/core/assets";
 import {registry} from "@web/core/registry";
-const {onWillStart, markup, Component, onMounted, onPatched, useRef} = owl;
+import {
+    Component,
+    markup,
+    onMounted,
+    onPatched,
+    onWillStart,
+    useRef,
+    useState,
+} from "@odoo/owl";
 
 export default class BokehChartJsonWidget extends Component {
     setup() {
-        this.widget = useRef("widget");
-        onPatched(() => {
-            var script = document.createElement("script");
-            script.text = this.props.value.script;
-            this.widget.el.append(script);
+        this.widget = useRef("bokeh");
+        this.state = useState({
+            oldScript: "",
         });
-        onMounted(() => {
-            var script = document.createElement("script");
-            script.text = this.props.value.script;
-            this.widget.el.append(script);
-        });
-        onWillStart(() =>
-            loadBundle({
+        onWillStart(async () => {
+            await loadBundle({
                 jsLibs: [
                     "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-3.1.1.min.js",
                     "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-api-3.1.1.min.js",
@@ -27,12 +28,27 @@ export default class BokehChartJsonWidget extends Component {
                     "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-mathjax-3.1.1.min.js",
                     "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-gl-3.1.1.min.js",
                 ],
-            })
-        );
+            });
+        });
+        onMounted(() => {
+            this.updateBokeh(this.props.value);
+        });
+        onPatched(() => {
+            this.updateBokeh(this.props.value);
+        });
     }
-    markup(value) {
-        console.log("Marking up...");
-        return markup(value);
+    updateBokeh(value) {
+        if (!value || value.script === this.oldScript) {
+            return;
+        }
+        const div = value.div || "";
+        const script = markup(value.script) || "";
+
+        if (this.widget.el) {
+            this.widget.el.innerHTML = div;
+            new Function(script)();
+        }
+        this.oldScript = script;
     }
 }
 
