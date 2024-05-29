17cad2424d0f20a27b9757da4967f74ef5c392cd
MariaBForgeFlow <maria.buscalla@forgeflow.com>
2024-05-29 10:20:15 +0200

    [IMP] web_widget_datatables: display query results on datatable

--- web_widget_datatables/static/src/js/web_widget_datatables.esm.js
+++ web_widget_datatables/static/src/js/web_widget_datatables.esm.js
@@ -4,12 +4,13 @@ import {CharField} from "@web/views/fields/char/char_field";
 import {loadBundle} from "@web/core/assets";
 import {registry} from "@web/core/registry";
 import {markup, onMounted, onPatched, onWillStart, useRef} from "@odoo/owl";
+import {useService} from "@web/core/utils/hooks";
 
 export default class DatatablesWidget extends CharField {
     setup() {
-
         super.setup();
         this.widget = useRef("widget");
+        this.orm = useService("orm");
 
         onWillStart(() => {
             loadBundle({
@@ -30,37 +31,39 @@ export default class DatatablesWidget extends CharField {
         onMounted(() => {
             this.updateDatatables(this.props.record.data[this.props.name]);
         });
-    };
+    }
 
-    updateDatatables(value) {
+    async updateDatatables() {
         // Clear previous table if it exists
         if (this.widget.el) {
-            this.widget.el.innerHTML = '';
-        }
-        var table = document.createElement('table');
-        table.id = 'example';
-        table.className = 'display';
-        table.width = '100%';
-        this.widget.el.appendChild(table); // Append table to the widget element
-        const dataSet = [
-            ['Tiger Nixon', 'System Architect', 'Edinburgh', '5421', '2011/04/25', '$320,800'],
-            ['Garrett Winters', 'Accountant', 'Tokyo', '8422', '2011/07/25', '$170,750'],
-            ['Ashton Cox', 'Junior Technical Author', 'San Francisco', '1562', '2009/01/12', '$86,000'],
-            ['Cedric Kelly', 'Senior Javascript Developer', 'Edinburgh', '6224', '2012/03/29', '$433,060'],
-            ['Airi Satou', 'Accountant', 'Tokyo', '5407', '2008/11/28', '$162,700'],
-        ];
+            this.widget.el.innerHTML = "";
 
-        $(table).DataTable({
-            columns: [
-                { title: 'Name' },
-                { title: 'Position' },
-                { title: 'Office' },
-                { title: 'Extn.' },
-                { title: 'Start date' },
-                { title: 'Salary' }
-            ],
-            data: dataSet
-        });
+            // Create table element
+            var table = document.createElement("table");
+            table.id = "example";
+            table.className = "display";
+            table.width = "100%";
+            this.widget.el.appendChild(table); // Append table to the widget element
+
+            // Fetch query results JSON from the record
+            const queryResultsJson = await this.orm.call(
+                "sql.export",
+                "get_sql_query_data",
+                [this.props.record.resId]
+            );
+
+            console.log("RPC Response:", queryResultsJson); // Log response to console
+
+            // Parse query results JSON
+            const queryResults = JSON.parse(queryResultsJson);
+            const columns = queryResults.header.map((title) => ({title}));
+            const data = Object.values(queryResults.rows);
+
+            $(table).DataTable({
+                columns: columns,
+                data: data,
+            });
+        }
     }
 }
 
